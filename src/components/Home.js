93e4abb73c292c20fe043b1cee23d0aed6202a81
Sasikant <nairs999@gmail.com>
2019-04-14 12:34:09 +0530

    Add CSS Reset and Nav menu

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,29 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        basic schema for react-redux 
+        <header>
+          <nav className="nav">
+            <div className="nav-logo">
+              <h1>conduit</h1>
+            </div>
+            <div className="nav-links">
+              <ul>
+                <li>Home</li>
+                <li>Sign in</li>
+                <li>Sign up</li>
+              </ul>
+        
+            </div>
+          </nav>  
+        </header>
+
+        {/* Hero section */}
+        <section>
+          <div className="hero">
+            <h2>conduit</h2>
+            <p>A place to share your knowledge.</p>
+          </div>
+        </section>
       </div>
     )
   }
