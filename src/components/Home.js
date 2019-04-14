cd587e7d971ff2db3926f7a4111d30b1b87af884
Sasikant <nairs999@gmail.com>
2019-04-14 14:00:03 +0530

    Add Hero and Post section

--- src/components/Home.js
+++ src/components/Home.js
@@ -27,6 +27,52 @@ export default class Home extends Component {
             <p>A place to share your knowledge.</p>
           </div>
         </section>
+        {/* Global Feed section */}
+        <section>
+          <div className="super">
+            <article className="article-wrapper">
+              {/* conditional rendering for tag required */}
+              <h2>Global Feed</h2>
+              <div className="post">
+
+                <div className="post-header">
+                  <div className="post-details">
+                    <img src="https://static.productionready.io/images/smiley-cyrus.jpg" />
+                    <div className="user-info">
+                      <h4>@UserName</h4>
+                      <p>post date</p>
+                    </div>
+                  </div>
+                  {/* Heart */}
+                  <div className="likes">
+                    <p className="heart">&#9829;</p>
+                    <p>0</p>
+                  </div>
+                </div>
+                
+                {/* Post content */}
+                <div className="post-content">
+                  <p>Article Name</p>
+                  <p>Article description</p>
+                  <button>Read More</button>
+
+                </div>
+              </div>
+            </article>
+
+
+          {/* Tag */}
+
+          <section>
+            <div className="tag">
+              <h4>Popular Tags</h4>
+              
+            </div>
+          </section>
+          </div>
+
+
+        </section>
       </div>
     )
   }
