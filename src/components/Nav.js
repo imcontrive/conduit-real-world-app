5428d3dc1f25a645447c47dbbc1086df80e5e82b
Sasikant <nairs999@gmail.com>
2019-04-14 17:18:03 +0530

    Correct Header and Nav menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,20 +3,35 @@ import {NavLink} from 'react-router-dom';
 
 export default function Nav() {
 	return (
-		<ul className="nav">
-			<li>
-				<NavLink exact activeClassName = 'active  ' className="navigation-link" to='/' >
-					Home
+		 <header>
+			<nav className="nav">
+				
+				<NavLink exact className="navigation-link nav-logo" to='/' >
+					<h5>conduit</h5>
 				</NavLink>
-			</li>
-			<li>
-				<NavLink exact activeClassName = 'active ' className="navigation-link" to='/register' >
-					Signup
-				</NavLink>
-				<NavLink exact activeClassName = 'active ' className="navigation-link" to='/login' >
-					Signin
-				</NavLink>
-			</li>
-		</ul>
+					
+		
+				<div className="nav-links">
+				<ul>
+					<li>
+						<NavLink exact activeClassName = 'active' className="navigation-link" to='/' >
+							Home
+						</NavLink>
+					</li>
+					<li>
+						<NavLink exact activeClassName = 'active' className="navigation-link" to='/register' >
+							Signup
+						</NavLink>
+						<NavLink exact activeClassName = 'active' className="navigation-link" to='/login' >
+							Signin
+						</NavLink>
+					</li>
+				</ul>
+		
+				</div>
+			</nav>  
+		</header> 
+				
+		
 	)
 }
